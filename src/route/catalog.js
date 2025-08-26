e630c3057870fdcc5bfe90a955c9353d630a545c
Giovanni Spinelli <38455598+giovannispinelli@users.noreply.github.com>
2025-08-26 16:34:38 +0200

    Update catalog.js

--- src/route/catalog.js
+++ src/route/catalog.js
@@ -1,17 +1,15 @@
 import express from 'express';
+import { productsCollection } from '../services/mongo.js';
+
 const router = express.Router();
 
-// Catalogo finto
-const products = [
-  { id: 1, name: "Laptop", price: 1200, stock: 10 },
-  { id: 2, name: "Smartphone", price: 800, stock: 25 },
-  { id: 3, name: "Cuffie Wireless", price: 150, stock: 50 },
-  { id: 4, name: "Monitor 27\"", price: 300, stock: 7 }
-];
-
-// GET /catalog → restituisce tutti i prodotti
-router.get('/', (req, res) => {
-  res.json(products);
+router.get('/', async (req, res) => {
+  try {
+    const products = await productsCollection.find({}).toArray();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: 'Errore nel recupero prodotti' });
+  }
 });
 
 export default router;
